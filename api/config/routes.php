11063aca16291353ae356d0ec01d1336b12fda7e
aloefflerj <aloeffler.j@gmail.com>
2024-07-10 17:10:20 -0300

    :sparkles: feat: allows 'debug' header

--- api/config/routes.php
+++ api/config/routes.php
@@ -19,7 +19,7 @@ $app->add(function ($request, $handler) {
     $response = $handler->handle($request);
     return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, debug')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
 
