cfcf5b791778fed11e0c0b3ee8977cd595b1f396
aloefflerj <aloeffler.j@gmail.com>
2024-07-09 23:15:33 -0300

    :sparkles: feat: adds svelte box to client

---
+++ client/src/components/svelte/SvelteBox.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    export let title: string;
+</script>
+
+<div class="svlete-box">
+    <div class="header">
+        <h2>{ title }</h2>
+    </div>
+    <slot />
+</div>
+
+<style scoped>
+    h2 {
+        padding: 0;
+        margin: 0;
+        color: #812d18;
+    }
+    div.header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 10%;
+        padding: 0;
+        margin: 0;
+        background-color: #cc6146;
+    }
+    div.svlete-box {
+        background-color: #d48875;
+        width: 100%;
+        height: 100%;
+    }
+</style>
