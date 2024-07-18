3c4aba09eae19278fa95ec53d15308dafd553063
aloefflerj <aloeffler.j@gmail.com>
2024-07-18 17:28:37 -0300

    :sparkles: feat: fetches texts from database and render them based on current step

--- client/src/hooks/useSteps.ts
+++ client/src/hooks/useSteps.ts
@@ -4,4 +4,5 @@ const routeBaseUrl = '/steps';
 
 export const useSteps = (callFromServer = false) => ({
     fetchSteps: (debug: boolean = false) => get(routeBaseUrl, debug, callFromServer),
+    fetchSpeechesByStep: (stepId: string, debug: boolean = false) => get(`${routeBaseUrl}/${stepId}/speeches`, debug, callFromServer),
 });
