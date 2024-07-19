de226ce380c7993f9631f83385a8d22859c8df13
aloefflerj <aloeffler.j@gmail.com>
2024-07-19 02:24:45 -0300

    :sparkles: feat: adds client step ws on svelte box

--- client/src/components/svelte/StepController.svelte
+++ client/src/components/svelte/StepController.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
     import type Step from "../../types/Step";
     import { currentStep } from '../../service/Steps.service';
+    import { stepPossibleTitles } from "../../helpers/StepPossibleTitles";
+    import { useWebSocket } from "../../hooks/useWebSocket";
+    import { onMount } from "svelte";
 
     export let steps: Step[] = [];
 
-    const step = currentStep(steps);
+    let step = currentStep(steps);
+    
+    onMount(async () => {
+        const { onMessage } = useWebSocket('/steps/svelte');
+        onMessage((e) => {
+            step = JSON.parse(e.data.toString());
+        });
+	});
+
 </script>
 
-{#if step?.title === "prologue"}
+{#if step?.title === stepPossibleTitles.prologue}
     <h1>prologue</h1>
+{:else if step?.title === stepPossibleTitles.bigBang}
+    <h1>big bang</h1>
 {:else}
     <h1>not found</h1>
 {/if}
